a4b0bd528b689357fa7b2e736fe01540e94f4cc2
Erick <flpasana@outlook.com>
2022-05-20 09:53:33 +0800

    dynamic html

--- app.py
+++ app.py
@@ -13,5 +13,27 @@ app = Flask(__name__)
 def hello_world():
     return "Hello World! My 1st Web Application!"
     # start the development server using the run() method
+
+# dynamic route
+@app.route("/test/<query>")
+def search(query):
+    return query
+# def search():
+#     return "Hello dynamic!"
+
+@app.route("/integer/<int:value>")
+def int_type(value):
+    print(value + 1)
+    return "correct"
+@app.route("/float/<float:value>")
+def float_type(value):
+    print(value + 1.0)
+    return "correct"
+# dynamic route that accepts slashes
+@app.route("/path/<path:value>")
+def path_type(value):
+    print(value)
+    return "correct"
+
 if __name__ == "__main__":
     app.run()

c4a5fc84e2b959d548d32992aced237153e6fb68
Erick <flpasana@outlook.com>
2022-05-23 16:12:15 +0800

    continuation

--- app.py
+++ app.py
@@ -5,14 +5,18 @@ from flask import Flask
 
 # create the application object
 app = Flask(__name__)
+
+# error handling
+app.config["DEBUG"] = True
+
 # use the decorator pattern to
 # link the view function to a url
 @app.route("/")
 @app.route("/hello")
+
 # define the view using a function, which returns a string
 def hello_world():
     return "Hello World! My 1st Web Application!"
-    # start the development server using the run() method
 
 # dynamic route
 @app.route("/test/<query>")
@@ -29,11 +33,19 @@ def int_type(value):
 def float_type(value):
     print(value + 1.0)
     return "correct"
+    
 # dynamic route that accepts slashes
 @app.route("/path/<path:value>")
 def path_type(value):
     print(value)
     return "correct"
+@app.route("/name/<name>")
+def index(name):
+    if name.lower() == "michael" :
+        return "Hello, {}".format(name), 200
+    else :
+        return "Not Found", 404
 
+# start the development server using the run() method
 if __name__ == "__main__":
     app.run()
